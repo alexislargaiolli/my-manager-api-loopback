fd72705170eb919b61053490d8fa03c20c00e172
Alexis Largaiolli <alexis.largaiolli@gmail.com>
2017-04-18 09:02:20 +0200

    Change procfile + update datasource config for production

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,10 +1,6 @@
 module.exports = {
     mysql: {
         connector: 'mysql',
-        hostname: process.env.DB_HOST,
-        port: process.env.DB_PORT || 27017,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        url: process.env.CLEARDB_DATABASE_URL
     }
 }
