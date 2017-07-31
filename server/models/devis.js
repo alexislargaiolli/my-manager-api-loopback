b0c2e0063590731e0a61891b06ac2f124a006a4d
Alexis Largaiolli <alexis.largaiolli@gmail.com>
2017-07-31 13:30:29 +0200

    Fix email link  + update devis model in order to query by user/:userId/devis  + Add project state message property  + Fix debug config

--- server/models/devis.js
+++ server/models/devis.js
@@ -1,5 +1,3 @@
 'use strict';
 
-module.exports = function(Devis) {
-
-};
+module.exports = function (Devis) {};
--- server/server.js
+++ server/server.js
@@ -9,26 +9,31 @@ var app = module.exports = loopback();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.start = function() {
-    var port = process.env.PORT || 3000
-        // start the web server
-    return app.listen(port, function() {
-        app.emit('started');
-        var baseUrl = app.get('url').replace(/\/$/, '');
-        console.log('Web server listening at: %s', baseUrl);
-        if (app.get('loopback-component-explorer')) {
-            var explorerPath = app.get('loopback-component-explorer').mountPath;
-            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-        }
-    });
+app.use(loopback.token({
+  model: app.models.accessToken,
+  currentUserLiteral: 'me'
+}));
+
+app.start = function () {
+  var port = process.env.PORT || 3000
+  // start the web server
+  return app.listen(port, function () {
+    app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-    if (err) throw err;
+boot(app, __dirname, function (err) {
+  if (err) throw err;
 
-    // start the server if `$ node server.js`
-    if (require.main === module)
-        app.start();
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
 });
